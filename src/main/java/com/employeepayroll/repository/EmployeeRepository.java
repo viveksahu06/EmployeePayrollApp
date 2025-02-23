b46001847dbc438d5be7d26bbd748d39a57c65ca
viveksahu06 <viveksahu1006@gmail.com>
2025-02-23 11:55:28 +0530

    [Vivek] Add. setup added for employee payroll system

---
+++ src/main/java/com/employeepayroll/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.employeepayroll.repository;
+
+import com.employeepayroll.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
